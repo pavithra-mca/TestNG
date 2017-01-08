9d9320d6c4a187734c8b6e179a3924d1be2fbcd1
Pavithra <pinkir2085@gmail.com>
2017-01-08 10:31:33 -0400

    First testcase uploaded in gits hub

---
+++ src/TestNGEx/FirstTestNG.java
@@ -0,0 +1,25 @@
+package TestNGEx;
+
+import org.testng.Reporter;
+import org.testng.annotations.*;
+public class FirstTestNG {
+	
+	@BeforeClass
+	public void setup()
+	{
+		Reporter.log("hi");
+		System.out.println("In setup");
+	}
+	
+	@Test
+	public void first(){
+		Reporter.log("In test",true);
+		System.out.println("Hello");
+	}
+	@AfterClass
+	public void teardown(){
+		Reporter.log("In tear down");
+		System.out.println("In Afterclass");
+	}
+
+}
